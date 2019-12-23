bc5147cc96254c47c07ad909e9454faf0761bcd6
galvarez <galvarez@intergrupo.com>
2019-12-23 14:21:12 -0500

    loading and reading images from browser with tesseract.js

---
+++ index.js
@@ -0,0 +1,29 @@
+import { createWorker } from "tesseract.js";
+
+window.onload = function() {
+  const worker = createWorker({
+    langPath: "https://tessdata.projectnaptha.com/4.0.0"
+  });
+
+  const textH1 = document.getElementById("text");
+  const fileInput = document.getElementById("file");
+
+  fileInput.addEventListener("change", event => {
+    const file = event.target.files[0];
+    textH1.innerText = "Cargando...";
+    document.getElementById("image").src = window.URL.createObjectURL(file);
+
+    load(textH1, file);
+  });
+
+  async function load(textH1, file) {
+    await worker.load();
+    await worker.loadLanguage("eng");
+    await worker.initialize("eng");
+    const {
+      data: { text }
+    } = await worker.recognize(file);
+    textH1.innerText = text;
+    await worker.terminate();
+  }
+};
